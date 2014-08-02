ac6998c9d5de5683bc55cc65c3a3633cd609b8b1
Olivia <oliviaslau@gmail.com>
2014-08-01 17:25:10 -0700

    exploring how to extract headline info

--- scrapeWeb.R
+++ scrapeWeb.R
@@ -12,6 +12,61 @@
 library(XML)
 library(twitteR)
 
+##htmlToText function adapted from github.com/tonybreyal
+htmlToText <- function(input, ...) {
+  ###---PACKAGES ---###
+  require(RCurl)
+  require(XML)
+  
+  
+  ###--- LOCAL FUNCTIONS ---###
+  # Determine how to grab html for a single input element
+  evaluate_input <- function(input) {    
+    # if input is a .html file
+    if(file.exists(input)) {
+      char.vec <- readLines(input, warn = FALSE)
+      return(paste(char.vec, collapse = ""))
+    }
+    
+    # if input is html text
+    if(grepl("</html>", input, fixed = TRUE)) return(input)
+    
+    # if input is a URL, probably should use a regex here instead?
+    if(!grepl(" ", input)) {
+	      # downolad SSL certificate in case of https problem
+      if(!file.exists("cacert.perm")) download.file(url="http://curl.haxx.se/ca/cacert.pem", destfile="cacert.perm")
+      return(getURL(input, followlocation = TRUE, cainfo = "cacert.perm"))
+    }
+    
+    # return NULL if none of the conditions above apply
+    return(NULL)
+  }
+  
+  # convert HTML to plain text
+  convert_html_to_text <- function(html) {
+    doc <- htmlParse(html, asText = TRUE)
+    text <- xpathSApply(doc, "//text()[not(ancestor::script)][not(ancestor::style)][not(ancestor::noscript)][not(ancestor::form)]", xmlValue)
+    return(text)
+  }
+  
+  # format text vector into one character string
+  collapse_text <- function(txt) {
+    return(paste(txt, collapse = " "))
+  }
+  
+  ###--- MAIN ---###
+  # STEP 1: Evaluate input
+  html.list <- lapply(input, evaluate_input)
+  
+  # STEP 2: Extract text from HTML
+  text.list <- lapply(html.list, convert_html_to_text)
+  
+  # STEP 3: Return text
+  text.vector <- sapply(text.list, collapse_text)
+  return(text.vector)
+}
+
+
 #list of interesting companies
 companies = c('Facebook', 'Twitter', 'Pandora', 'Apple', 'Google', 'Zynga', 'Tesla', 'Yahoo')
 #list of URLs for each company name
@@ -24,3 +79,15 @@ table.info = lapply(
 										'[', 2)				#2nd column values
 #get info of interest: sector, industry, employees - respectively
 actual.interested.values = data.frame(lapply(table.info, function(x) x[6:8,]))
+colnames(actual.interested.values) <- companies
+
+##Scrape websites for recent headline information
+#Let's explore where the headlines are...
+x = readLines("http://finance.yahoo.com/q/h?s=TSLA")
+grep('August 1', x)
+x[183]
+###Hmm, this line is an entire block of all headlines for the day.
+#use HTML to text function (above)
+x = htmlToText("http://finance.yahoo.com/q/h?s=TSLA")
+
+
